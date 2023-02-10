7f115336b4d634af9dd754219967b7898b5bd415
Klein Tahiraj <klein.tahiraj@gmail.com>
2023-02-10 17:32:33 +0100

    fixing regex filter, fixing self.type redundancy

--- KPA.py
+++ KPA.py
@@ -9,10 +9,13 @@ from nltk.tokenize import wordpunct_tokenize
 class KPA():
     def __init__(self):
         self.download_types = ['arguments', 'key_points', 'labels']
+        self.types = self.download_types[:-1]
         self.datasets = ['train', 'dev', 'test']
+
         self.raw_dataframes = {f"{type}_{dataset}": self.__download_files(type, dataset) for type in self.download_types for dataset in self.datasets}
         self.processed = False
         self.tokenized = False
+
 
     def __download_files(self, type, dataset):
             if dataset == 'test':
@@ -24,36 +27,38 @@ class KPA():
 
 
     def __preprocess(self, text):
-        allowed_chars = re.compile('[^0-9a-z #+_]')
-        text = re.sub(allowed_chars, '', text)
+        allowed_chars = re.compile('[^a-zA-Z]')
+        text = re.sub(allowed_chars, ' ', text)
         return text.lower()
 
 
     def __text_processing(self):
-        for index in [f"{type}_{dataset}" for dataset in self.datasets for type in ['arguments','key_points']]:
+        for index in [f"{type}_{dataset}" for dataset in self.datasets for type in self.types]:
             dataframe = self.raw_dataframes[index]
             for column in dataframe.columns[1:3]:
                 dataframe[column] = dataframe[column].apply(self.__preprocess)
         self.processed = True
 
+
     # in the other functions I pass A dataframe, here I pass ALL raw_dataframes, which approach is better and more intuitive? # TODO
     def __tokenize(self):
         self.dataframes = copy.deepcopy(self.raw_dataframes)
-        for index in [f"{type}_{dataset}" for dataset in self.datasets for type in ['arguments','key_points']]:
+        for index in [f"{type}_{dataset}" for dataset in self.datasets for type in self.types]:
             dataframe = self.dataframes[index]
             for column in dataframe.columns[1:3]:
                 dataframe[column] = dataframe[column].apply(wordpunct_tokenize)
         self.tokenized = True
 
+
     #it is just incredibly fast
     def processing(self):
         self.__text_processing()
         self.__tokenize()
-        print('hello')
+        print('hello c:')
 
 
 if __name__ == "__main__":
-    print('hello')
+    print('hello main c:')
     #instance = KPA()
     #instance.processing()
     #print(instance.raw_raw_dataframes.keys())
