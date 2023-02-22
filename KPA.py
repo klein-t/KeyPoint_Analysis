08d65c13e8b066d3350c8424e4cf7ba539cde7b5
Klein Tahiraj <klein.tahiraj@gmail.com>
2023-02-22 13:58:52 +0100

    Modularity. OOVs handling.

--- KPA.py
+++ KPA.py
@@ -4,17 +4,21 @@ import re
 import copy
 import nltk
 from nltk.tokenize import wordpunct_tokenize
+from utils import padding
 
  
-class KPA():
+class data():
     def __init__(self):
         self.download_types = ['arguments', 'key_points', 'labels']
         self.types = self.download_types[:-1]
         self.datasets = ['train', 'dev', 'test']
+        self.train_dev = self.datasets[:-1]
 
         self.raw_dataframes = {f"{type}_{dataset}": self.__download_files(type, dataset) for type in self.download_types for dataset in self.datasets}
         self.processed = False
         self.tokenized = False
+
+        self.vocab = {"id2word": {}, "word2id": {}, "vocabs": []}
 
 
     def __download_files(self, type, dataset):
@@ -26,22 +30,22 @@ class KPA():
             return data
 
 
-    def __preprocess(self, text):
+    def preprocess(self, text):
         allowed_chars = re.compile('[^a-zA-Z]')
         text = re.sub(allowed_chars, ' ', text)
         return text.lower()
 
 
-    def __text_processing(self):
+    def text_processing(self):
         for index in [f"{type}_{dataset}" for dataset in self.datasets for type in self.types]:
             dataframe = self.raw_dataframes[index]
             for column in dataframe.columns[1:3]:
-                dataframe[column] = dataframe[column].apply(self.__preprocess)
+                dataframe[column] = dataframe[column].apply(self.preprocess)
         self.processed = True
 
 
     # in the other functions I pass A dataframe, here I pass ALL raw_dataframes, which approach is better and more intuitive? # TODO
-    def __tokenize(self):
+    def tokenize(self):
         self.dataframes = copy.deepcopy(self.raw_dataframes)
         for index in [f"{type}_{dataset}" for dataset in self.datasets for type in self.types]:
             dataframe = self.dataframes[index]
@@ -49,11 +53,63 @@ class KPA():
                 dataframe[column] = dataframe[column].apply(wordpunct_tokenize)
         self.tokenized = True
 
+    def build_vocab(self):
+        word_id = 1
+        for index in [f"{type}_{dataset}" for dataset in self.datasets[:-1] for type in self.types]:
+            dataframe = self.dataframes[index]
+            for column in dataframe.columns[1:3]:
+                for sentence in dataframe[column]:
+                    for word in sentence:
+                        if word not in self.vocab['word2id'].keys():
+                            self.vocab['word2id'][word] = word_id
+                            self.vocab['id2word'][word_id] = word
+                            word_id += 1
+        self.vocab['vocabs'] = list(self.vocab['word2id'].keys())
 
-    #it is just incredibly fast
+    def encoding(self):
+        self.encoded_dataframes = copy.deepcopy(self.dataframes)
+        for index in [f"{type}_{dataset}" for dataset in self.datasets for type in self.types]:
+            dataframe = self.encoded_dataframes[index]
+            for column in dataframe.columns[1:3]:
+                dataframe[column] = dataframe[column].apply(
+                    lambda sentence: [self.vocab['word2id'].get(word, 0) for word in sentence])
+
+
+    def get_longest_sentence(self):
+        self.max_len = 0
+        for index in [f"arguments_{dataset}" for dataset in self.train_dev]:
+            dataframe = self.encoded_dataframes[index]
+            for column in dataframe.columns[1:3]:
+                for sentence in dataframe[column]:
+                    self.max_len = max(self.max_len, len(sentence))
+
+    def alligment(self, id, dataset):
+        if id in self.encoded_dataframes[f'arguments_{dataset}']['arg_id'].to_numpy():
+            return self.encoded_dataframes[f'arguments_{dataset}'].loc[self.encoded_dataframes[f'arguments_{dataset}']['arg_id'] == id, 'argument'].iloc[0]
+        elif id in self.encoded_dataframes[f'key_points_{dataset}']['key_point_id'].to_numpy():
+            return self.encoded_dataframes[f'key_points_{dataset}'].loc[self.encoded_dataframes[f'key_points_{dataset}']['key_point_id'] == id, 'key_point'].iloc[0]
+
+
+    def allign(self):
+        self.alligned_dataframes = {}
+        for dataset in self.datasets:
+            self.alligned_dataframes[dataset] = self.dataframes[f'labels_{dataset}'][[
+                'arg_id', 'key_point_id', 'label']]
+            self.alligned_dataframes[dataset][['encoded_arg', 'encoded_kp']] = self.alligned_dataframes[dataset][[
+                'arg_id', 'key_point_id']].applymap(lambda id: self.alligment(id, dataset))
+            self.alligned_dataframes[dataset][['encoded_arg', 'encoded_kp']] = self.alligned_dataframes[dataset][[
+                'encoded_arg', 'encoded_kp']].applymap(lambda lst: padding(lst, self.max_len))
+
+# ['index'][['encoded_arg', 'encoded_kp']].applymap(lambda lst: padding(lst, self.max_len))
+
     def processing(self):
-        self.__text_processing()
-        self.__tokenize()
+        self.text_processing()
+        self.tokenize()
+        self.build_vocab()
+        self.encoding()
+        self.get_longest_sentence()
+        self.allign()
+        self.processed = True
 
 
 if __name__ == "__main__":
--- glove.py
+++ glove.py
@@ -2,17 +2,17 @@ import gensim
 import numpy as np
 import gensim.downloader as gloader
 import KPA
-from KPA import KPA
+from KPA import data
 import copy
 import pandas as pd
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.metrics import  classification_report, PrecisionRecallDisplay, roc_curve, balanced_accuracy_score, ConfusionMatrixDisplay
 from sklearn.metrics import classification_report, average_precision_score, PrecisionRecallDisplay, ConfusionMatrixDisplay
 import os
+from utils import unpack
 
 
-
-class glove(KPA):
+class glove(data):
     def __init__(self, set = 'test', embedding_space_dimension = 300):
         super().__init__()
 
@@ -30,22 +30,9 @@ class glove(KPA):
 
         self.vocab = {"id2word": {}, "word2id": {}, "vocabs":[]}
 
-    # extract all sentences, build the set of tokens, assign to each an id
-    def build_vocab(self):
-        word_id = 0
-        for index in [f"{type}_{dataset}" for dataset in self.datasets for type in self.types]:
-            dataframe = self.dataframes[index]
-            for column in dataframe.columns[1:3]:
-                for sentence in dataframe[column]:
-                    for word in sentence:
-                        if word not in self.vocab['word2id'].keys():
-                            self.vocab['word2id'][word] = word_id
-                            self.vocab['id2word'][word_id] = word
-                            word_id += 1
-        self.vocab['vocabs'] = list(self.vocab['word2id'].keys())
 
     def build_embedding_matrix(self):
-        embedding_matrix_shape = (len(self.vocab['vocabs']), self.emb_dim) 
+        embedding_matrix_shape = (len(self.vocab['vocabs']) + 1, self.emb_dim) 
         self.embedding_matrix = np.zeros(shape = embedding_matrix_shape, dtype = np.float32) 
         for word, idx in self.vocab['word2id'].items():
             try:
@@ -54,47 +41,11 @@ class glove(KPA):
                 embedded_word = np.random.uniform(low = -0.1, high = 0.1, size = self.emb_dim)
             self.embedding_matrix[idx] = embedded_word
 
-    def encoding(self):
-        self.encoded_dataframes = copy.deepcopy(self.dataframes)
-        for index in [f"{type}_{dataset}" for dataset in self.datasets for type in self.types]:
-            dataframe = self.encoded_dataframes[index]
-            for column in dataframe.columns[1:3]:
-                dataframe[column] = dataframe[column].apply(lambda sentence: [self.vocab['word2id'][words] for words in sentence])
-
-    def get_longest_sentence(self):
-        self.max_len = 0
-        for index in [f"arguments_{dataset}" for dataset in ['train', 'dev']]:
-            dataframe = self.encoded_dataframes[index]
-            for column in dataframe.columns[1:3]:
-                    for sentence in dataframe[column]:
-                        self.max_len = max(self.max_len, len(sentence))
-
-    def alligment(self,id):
-        if id in self.encoded_dataframes[f'arguments_{self.set}']['arg_id'].to_numpy():
-            return self.encoded_dataframes[f'arguments_{self.set}'].loc[self.encoded_dataframes[f'arguments_{self.set}']['arg_id'] == id, 'argument'].iloc[0]
-        elif id in self.encoded_dataframes[f'key_points_{self.set}']['key_point_id'].to_numpy():
-            return self.encoded_dataframes[f'key_points_{self.set}'].loc[self.encoded_dataframes[f'key_points_{self.set}']['key_point_id'] == id, 'key_point'].iloc[0]
-
-
-    def unpack(self, data):
-        for i, arr in enumerate(data):
-            data[i] = arr[0][0]
-        return data
-
-    def padding(self, lst):
-        return lst + [0] * (self.max_len - len(lst))
-
-
     def processing(self):
         if not self.processed:
             super().processing()
-        self.build_vocab()
         self.build_embedding_matrix()
-        self.encoding()
-        self.get_longest_sentence()
-        self.alligned_dataframe = self.dataframes[f'labels_{self.set}'][['arg_id', 'key_point_id']]
-        self.alligned_dataframe[['encoded_arg_id', 'encoded_key_point_id']] = self.dataframes[f'labels_{self.set}'][['arg_id', 'key_point_id']].applymap(self.alligment)
-        self.alligned_dataframe[['encoded_arg_id', 'encoded_key_point_id']] = self.alligned_dataframe[['encoded_arg_id', 'encoded_key_point_id']].applymap(self.padding)
+
 
     def measure(self):
         self.scores = {}
@@ -103,7 +54,7 @@ class glove(KPA):
 
         self.scores['list'] = []
 
-        for _, row in self.alligned_dataframe.iterrows():
+        for _, row in self.alligned_dataframes[self.set].iterrows():
             encoded_arg = self.embedding_matrix[row['encoded_arg_id']]
             encoded_kp = self.embedding_matrix[row['encoded_key_point_id']]
 
@@ -116,7 +67,8 @@ class glove(KPA):
             self.scores['sum'][row['arg_id']] = {row['key_point_id'] : similarity_summed}
             self.scores['list'].append(similarity_summed)
 
-        self.scores['list'] = np.asarray(self.unpack(self.scores['list']))
+        self.scores['list'] = np.asarray(unpack(self.scores['list']))
+        print(f'we are evaluating similarity among arguments and key points in the {self.set} set')
 
 
     def evaluate(self): 
